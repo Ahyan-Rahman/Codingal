80d62dd43350d057962220db3c5b9c7518c55ccb
Ahyan-Rahman <ahyanrahman100@gmail.com>
2024-12-23 17:24:06 +0800

    Hi

--- Activity 1.py
+++ Activity 1.py
@@ -1,1 +1,18 @@
-print('hello!')
+def totalSetBits(n):
+    # Count variable set as 0 
+    
+    ones=0
+    zeros=0
+    # Right shift the number till we find first set bit
+    while (n):
+        if(n&1==1):
+            ones+=1
+        else:
+            zeros+=1
+        n >>= 1 
+
+    print('The number of Zeros',zeros)
+    print('The number of ones',ones)      
+
+number= int(input("Enter a number: "))
+totalSetBits(number)
